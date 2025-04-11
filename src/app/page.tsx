c40c9488046b6b0f1d046e1cc8a798b03c9aa72e
Estevão <estevaobresolin@hotmail.com>
2025-04-11 14:28:51 -0300

    update section1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,4 @@
 // src/app/page.jsx
-import Link from 'next/link';
-import Header from '@/app/components/header'
-import Section1 from '@/app/components/section1'
-import Section2 from '@/app/components/section2'
-import Section3 from '@/app/components/section3'
-import Section4 from '@/app/components/section4'
-import Section5 from '@/app/components/section5'
-import Section6 from '@/app/components/section6'
-import Section7 from '@/app/components/section7'
 import Gerenciador from '@/app/gerenciador/page'
 // ...
 
